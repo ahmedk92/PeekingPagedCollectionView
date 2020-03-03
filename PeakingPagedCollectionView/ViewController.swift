b20743e72c3b992debbb708f8d94c81e6a980d3d
Ahmed Khalaf <ahmedkhalaf.92@gmail.com>
2020-03-03 22:22:56 +0200

    Initial Commit

---
+++ PeakingPagedCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PeakingPagedCollectionView
+//
+//  Created by Ahmed Khalaf on 3/3/20.
+//  Copyright © 2020 io.github.ahmedkhalaf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
